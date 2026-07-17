e653056298c105ac6061ef8217e7f8daea99eabe
Cristopher Gabriel Vera Quispe <114591444+CristopherVQ1024@users.noreply.github.com>
2026-07-16 22:29:06 -0500

    Comisaría-odoo

--- colegio_colegiados/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import especialidad
-from . import colegiado
-from . import solicitud
-from . import documento
-from . import cuota
-from . import pago
--- colegio_colegiados/models/especialidad.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Especialidad(models.Model):
-    _name = 'colegio.especialidad'
-    _description = 'Especialidad'
-
-    name = fields.Char(string='Nombre', required=True)
-    descripcion = fields.Text(string='Descripción')
